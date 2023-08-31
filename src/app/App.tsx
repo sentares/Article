19eda50e75f1b01d41e3ad06e14844a71e9fc321
Aidar <aydararzybekov2002@mail.ru>
2023-08-31 15:40:48 +0600

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+
+
+const App = () => {
+   const { theme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </div>
+    );
+};
+
+export default App;
