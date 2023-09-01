f601301de4c1b192ee7c8092e59206fbc8535577
Aidar <aydararzybekov2002@mail.ru>
2023-09-01 17:51:08 +0600

    create eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extentions': 'off',
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    // 'react/react-in-jsx-scope': 'off',
+    'react/no-deprecated': 'off',
+    'react/function-component-definition': 'off',
+    '@typescript-eslint/indent': [2, 2],
+    indent: [2, 2],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-shadow': 'off',
+    'no-underscore-dangle': 'off',
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+}
--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,28 +1,27 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from './LangSwitcher.module.scss';
-import {useTranslation} from "react-i18next";
-import React from "react";
-import {Button, ThemeButton} from "shared/ui/Button/Button";
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './LangSwitcher.module.scss'
+import { useTranslation } from 'react-i18next'
+import React from 'react'
+import { Button, ThemeButton } from 'shared/ui/Button/Button'
 
 interface LangSwitcherProps {
-    className?: string;
+  className: string
 }
 
-export const LangSwitcher = ({className}: LangSwitcherProps) => {
-    const { t, i18n } = useTranslation();
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+  const { t, i18n } = useTranslation()
 
-    const toggle = async () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-    }
+  const toggle = async () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+  }
 
-    return (
-        <Button
-            className={classNames(cls.LangSwitcher, {}, [className])}
-            theme={ThemeButton.CLEAR}
-            onClick={toggle}
-        >
-            {t('Язык')}
-        </Button>
-    );
-};
-
+  return (
+    <Button
+      className={classNames(cls.LangSwitcher, {}, [className])}
+      theme={ThemeButton.CLEAR}
+      onClick={toggle}
+    >
+      {t('Язык')}
+    </Button>
+  )
+}
