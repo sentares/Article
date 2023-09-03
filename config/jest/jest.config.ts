5b6248dad8abdb3deec57cb7ed395065ac2659c2
Aidar <aydararzybekov2002@mail.ru>
2023-09-03 23:29:11 +0600

    add testing-library

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -20,7 +20,14 @@ export default {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
   moduleDirectories: ['node_modules'],
   rootDir: '../../',
+  modulePaths: ['<rootDir>src'],
   testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>config/jest/setUpTests.ts'],
+  moduleNameMapper: {
+    // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    //   '<rootDir>/__mocks__/fileMock.js',
+    '\\.s?css$': 'identity-obj-proxy',
+  },
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
