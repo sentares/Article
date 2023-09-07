7a8f28fa7da9bb935fb5474ea0950c0865c8b4e7
Aidar <aydararzybekov2002@mail.ru>
2023-09-07 21:12:41 +0600

    update componentRender

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit'
 import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
-import { createReduxStore } from '../config/store'
 import { StateSchema } from '../config/StateSchema'
+import { createReduxStore } from '../config/store'
 
 interface StoreProviderProps {
   children?: ReactNode
-  initialState?: StateSchema
+  initialState?: DeepPartial<StateSchema>
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
   const { children, initialState } = props
 
-  const store = createReduxStore(initialState)
+  const store = createReduxStore(initialState as StateSchema)
 
   return <Provider store={store}>{children}</Provider>
 }
