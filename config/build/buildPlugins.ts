69b5f3076ff41c0135194f16a65c80fd0465454e
Aidar <aydararzybekov2002@mail.ru>
2023-09-11 19:31:20 +0600

    update Dynamic Loader

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -19,14 +19,13 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
   ]
 
-  plugins.push(
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
-  )
-
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    )
   }
 
   return plugins
