a85b9399ebb3c1affad8d6d034c054755a15d16a
Aidar <aydararzybekov2002@mail.ru>
2023-09-13 17:24:17 +0600

    update tests

--- src/features/AuthByUsername/model/service/LoginByUsername.test.ts
+++ src/features/AuthByUsername/model/service/LoginByUsername.test.ts
@@ -1,5 +1,3 @@
-import { Dispatch } from '@reduxjs/toolkit'
-import { StateSchema } from 'app/providers/StoreProvider'
 import axios from 'axios'
 import { userActions } from 'entities/User'
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
