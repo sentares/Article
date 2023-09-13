6603ec9b095583c5133aed06ce585bfbf354c9ff
Aidar <aydararzybekov2002@mail.ru>
2023-09-13 18:31:13 +0600

    update sidebar and add memo to components

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Button, ButtonTheme } from 'shared/ui/Button/Button'
@@ -7,7 +8,7 @@ interface LangSwitcherProps {
   short?: boolean
 }
 
-export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
+export const LangSwitcher = memo(({ className, short }: LangSwitcherProps) => {
   const { t, i18n } = useTranslation()
 
   const toggle = async () => {
@@ -15,11 +16,8 @@ export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
   }
 
   return (
-    <Button className={classNames('', {}, [className])}
-      theme={ButtonTheme.CLEAR}
-      onClick={toggle}
-    >
+    <Button className={classNames('', {}, [className])} theme={ButtonTheme.CLEAR} onClick={toggle}>
       {t(short ? 'Короткий язык' : 'Язык')}
     </Button>
   )
-}
+})
